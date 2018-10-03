bd654598496675befc60e4482bf9d98233385418
kduong@stuy.edu <kduong@stuy.edu>
2018-10-03 12:43:39 -0400

    Completed and tested deposit function.

--- BankAccount.java
+++ BankAccount.java
@@ -26,11 +26,28 @@ public class BankAccount {
     password = str;
   }
 
+  //public void setBalance(double money) {
+  //balance += money;
+  //}
+
+  public boolean deposit(double depo) {
+    if (depo >= 0.0) {
+      balance += depo;
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   public static void main(String[]args){
 
 		BankAccount a = new BankAccount(3202.98, 100309, "cool");
     System.out.println("The ID of my bank account is " +a.getAccountID() + ". It has a balance of "+a.getBalance() + ".");
     System.out.println(a);
+    a.setPassword("hi123");
+    a.deposit(412.09);
+    System.out.println("My new balance is " + a.getBalance() + ".");
+    a.deposit(-52.09);
 
   }
 
